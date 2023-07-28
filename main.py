17620d73af2a9368513ce739c62081abb33f8ffb
RonaldM30 <140295585+RonaldM30@users.noreply.github.com>
2023-07-27 22:42:48 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# site scripts = https://cdnjs.com
+# pip install python-socketio flask-socketio simple-websocket
+
+# Frontent
+ # html, css, javascript
+
+# Backend -> lógica de funcionamento por trás do site
+ # python
+
+# Framework -> Flask -> criar site
+
+# ambiente virtual -> local no seu computador com instalações esp.
+
+        # Criar o Site
+
+# importe o flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+# crie o app
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# funcionalidade de enviar msg
+@socketio.on("message")
+def gerenciar_msg(mensagem):
+    send(mensagem, broadcast=True)
+
+# criar a 1 pagina = 1 rota
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+# roda o nosso app
+socketio.run(app, host="192.168.15.110")
+
+# websocket -> tunel
